b81195a961fdc4d1b15521b26e155dc2b7ff7d67
Crystal Danger <cdneal091@gmail.com>
2020-02-02 16:28:25 -0500

    add ablity to diplay thread previews

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
 import Thread from './Thread'
+import { Card } from 'react-bootstrap'
 
 const Board = props => {
+  const threadPreview = () => {
+    return (
+      <Card style={{width: '18rem'}}>
+        <Card.Img variant="top" src="https://i.ytimg.com/vi/-NMPLAZ5JBI/maxresdefault.jpg"/>
+        <Card.Body>
+          <Card.Title>
+            Im a thread preview!
+          </Card.Title>
+          <Card.Text>
+            lorem ipsum potatoeum
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    )
+  }
   return (
     <div>
-      {}
+      {threadPreview()}
     </div>
   )
 }
