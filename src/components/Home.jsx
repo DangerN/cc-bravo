153026afca2252b0800ffc5d70e554db0688fd9a
Crystal Danger <cdneal091@gmail.com>
2020-02-02 16:36:45 -0500

    add content to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
+import { Jumbotron } from "react-bootstrap"
+
 const Home = props => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <Jumbotron>
+        <h1>
+          Welcome to CloudChan!
+        </h1>
+        <p>
+          Where the facts are made up and the up votes don't matter.
+        </p>
+        <p>
+          We are working very hard to get this site off the ground. Check back again soon!
+        </p>
+      </Jumbotron>
+      Board list goes here.
+    </>
   )
 }
 
