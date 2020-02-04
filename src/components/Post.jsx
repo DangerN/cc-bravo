850c8d6a079dee67dce0ff77a5994cbf0aef268b
Crystal Danger <cdneal091@gmail.com>
2020-02-04 18:21:01 -0500

    posts now diplay image and text

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Card, Col } from 'react-bootstrap'
 
 const Post = props => {
   return (
-    <div>
-    </div>
+    <Card>
+      <Card.Body>
+      { props.image ? <img style={{float: "left"}}width={256} src={props.image}/> : null}
+        I am a post Lorem ipsum dolor amet prism leggings bitters vegan meggings raclette letterpress fingerstache sartorial. Polaroid taiyaki kitsch, godard sustainable lyft narwhal waistcoat venmo kombucha offal umami. Poutine raclette truffaut, gluten-free intelligentsia church-key venmo taxidermy tacos poke bicycle rights. Tbh beard you probably haven't heard of them, portland gentrify twee cornhole art party ramps kinfolk pinterest. Kale chips asymmetrical pitchfork bushwick. Neutra next level snackwave taiyaki, etsy activated charcoal quinoa subway tile roof party.
+
+Pabst pour-over vape vegan. Slow-carb master cleanse wolf jean shorts wayfarers palo santo chicharrones hot chicken readymade raw denim distillery. Truffaut meh kale chips offal, marfa tbh vinyl chia cardigan. Brooklyn distillery trust fund, meh shaman bicycle rights adaptogen. Tofu vaporware seitan, williamsburg everyday carry stumptown la croix hammock flexitarian narwhal.
+      </Card.Body>
+    </Card>
   )
 }
 
--- src/components/Thread.jsx
+++ src/components/Thread.jsx
@@ -3,9 +3,22 @@ import React from 'react'
 import Post from './Post'
 
 const Thread = props => {
+  const displayPosts = postData => {
+    console.log(postData);
+    console.log(postData.image);
+    return (
+      <Post image={postData.image}/>
+    )
+  }
   return (
     <div>
-      This is a thread
+      {displayPosts({image: "https://i.ytimg.com/vi/-NMPLAZ5JBI/maxresdefault.jpg"})}
+      {displayPosts({})}
+      {displayPosts({})}
+      {displayPosts({})}
+      {displayPosts({})}
+      {displayPosts({})}
+      {displayPosts({})}
     </div>
   )
 }
