22da991350fae130d2b98ca726aa6d904d3887c2
Crystal Danger <cdneal091@gmail.com>
2020-02-04 15:25:29 -0500

    enable dropdown links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Navbar, NavDropdown, Nav } from 'react-bootstrap'
+import { A } from 'hookrouter'
 
 const ChanNav = (props) => {
   return (
     <Navbar collapseOnSelect varient="dark" bg="light" expand="lg" sticky="top">
       <Navbar.Brand href="/">CloudChan</Navbar.Brand>
       <NavDropdown title="Boards" id="collasible-nav-dropdown">
-        <NavDropdown.Item>fit</NavDropdown.Item>
-        <NavDropdown.Item>pol</NavDropdown.Item>
-        <NavDropdown.Item>ck</NavDropdown.Item>
+        <NavDropdown.Item as={A} href="/fit">fit</NavDropdown.Item>
+        <NavDropdown.Item as={A} href="/pol">pol</NavDropdown.Item>
+        <NavDropdown.Item as={A} href="/ck">ck</NavDropdown.Item>
       </NavDropdown>
       <Navbar.Toggle/>
       <Navbar.Collapse >
