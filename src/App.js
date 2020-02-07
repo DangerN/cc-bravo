ed7a16a540ce088cc2369ddfa3b2fdd84ce6d8e4
Crystal Danger <cdneal091@gmail.com>
2020-02-07 13:09:07 -0500

    settings persist bewtween page loads

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Row, Col } from 'react-bootstrap'
 import { useRoutes, usePath } from 'hookrouter'
@@ -12,9 +12,7 @@ import Settings from './components/Settings'
 import {THEME} from './resources/constants'
 import useSettings from './hooks/useSettings'
 
-// import "bootswatch/dist/flatly/bootstrap.min.css"
-// import "bootswatch/dist/darkly/bootstrap.min.css"
-
+import "bootswatch/dist/flatly/bootstrap.min.css"
 
 const routes = {
   '/': () => props => <Home {...props} />,
@@ -23,8 +21,15 @@ const routes = {
 
 export default () => {
   const match = useRoutes(routes)
+
+  // Initalize the settings hook, retrieve settings from localStorage and apply storedSettings to the settings state.
   const [settingsState, settingsDispatch] = useSettings()
-  console.log(settingsState);
+  const storedSettings = JSON.parse(localStorage.getItem("cloudChanSettings"))
+  useEffect(() => {
+    const applyStoredSettings = () => settingsDispatch({type: 'loadStored', stored: storedSettings})
+    storedSettings && applyStoredSettings()
+  },[])
+
   return (
     <>
       { THEME[settingsState.theme] }
--- src/hooks/useSettings.jsx
+++ src/hooks/useSettings.jsx
@@ -1,19 +1,25 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
 
 const useSettings = () => {
   const initialState = {
     theme: "light",
   }
   const reducer = (state, action) => {
-    console.log(action);
     switch (action.type) {
       case 'setTheme':
         return {...state, theme: action.theme}
+      case 'loadStored':
+        return action.stored
       default:
         throw new Error("yikes")
     }
   }
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  //save the current state to localStorage every time it is updated.
+  useEffect(() => {
+    localStorage.setItem("cloudChanSettings", JSON.stringify(state))
+  },[state])
   return [state, dispatch]
 }
 
