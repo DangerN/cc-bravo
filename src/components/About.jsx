684bebd509b9ea5e7b87251bb9e2303d3845a70a
Crystal Danger <cdneal091@gmail.com>
2020-03-10 18:35:18 -0400

    add about page

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = props => {
+  return (
+    <div>
+      <h1>About</h1>
+      <h3>The Problem</h3>
+      <h3>Our Solution</h3>
+    </div>
+  )
+}
+
+export default About
